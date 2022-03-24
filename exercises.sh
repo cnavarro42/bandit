4ab2b5a62da81d83b311e017659e883abe264592
Carlos Navarro <cnavarro9205@gmail.com>
2022-03-24 16:28:30 +0100

    syntax fixed

--- exercises.sh
+++ exercises.sh
@@ -1,63 +1,98 @@
-#Bandit connect
+#!/bin/bash
+
+############ THIS IS NOT A SCRIPT BY ITSELF, JUST COMMANDS TO SOLVE EXERCISES ############
+
+#https://overthewire.org/wargames/bandit/
+
+            ####BANDIT CONNECT
+
 ssh bandit0@bandit.labs.overthewire.org -p 2220
 bandit0
-#Bandit Lvl 0
+
+            ####BANDIT LVL 0 <----
+
+#1
 cat readme
-#------
+#FLAG
 boJ9jbbUNNfktd78OOpsqOltutMc3MY1
-#Bandit Lvl 1
+
+            ####BANDIT LVL 1 <----
+    #1
 cat ./-
-#------
+    #2
 cat ./*
-#------
+    #3
 cat /home/bandit1/-
-#------
+    #FLAG
 CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
-#Bandit Lvl 2
+
+            ####BANDIT LVL 2 <----
+    #1
 cat spaces\ in\ this\ filename
-#------
+    #2
 cat "spaces in this filename"
-#------
+    #3
 cat 'spaces in this filename'
-#------
+#FLAG
 UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
-#Bandit Lvl 3
+
+            ####BANDIT LVL 3 <----
+    #1
 cat inhere/.hidden
-#------
+    #2
 cd inhere
 cat .hidden
-#------
+    #FLAG
 pIwrPrtPN36QITSp3EQaw936yaFoFgAB
-#Bandit Lvl 4
+
+            ####BANDIT LVL 4 <----
+
+    #1
 file inhere/*
 cat inhere/-file07
-#------
+    #2
 file inhere/* | grep ASCII | awk '{print $1}' | tr ':' ' ' | xargs cat
-#------
+    #FLAG
 koReBOKuIDDepwhWk7jZC0RTdopnAYKh
-#Bandit Lvl 5
+
+            ####BANDIT LVL 5 <----
+
+    #1
 find ./inhere -type f -readable -size 1033c ! -executable | xargs cat | sed 's/ *//'
-#------
+    #FLAG
 DXjZPULLxYr17uwoI01bNLQbtFemEgo7
-#Bandit Lvl 6
+
+            ####BANDIT LVL 6 <----
+
+    #1
 find / -user bandit7 -group bandit6 -size 33c -type f 2>/dev/null | xargs cat
-#------
+    #FLAG
 HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs
-#Bandit Lvl 7
+
+            ####BANDIT LVL 7 <----
+
+    #1
 grep "millionth" data.txt | awk '{print $2}'
-#------
+    #2
 grep "millionth" data.txt | awk 'NF{print $NF}'
-#------
+    #3
 awk '/millionth/' data.txt | awk '{print $2}'
-#------
+#FLAG
 cvX2JJa4CFALtqS87jk27qwqGhBM9plV
-#Bandit Lvl 8
+
+            ####BANDIT LVL 8 <----
+
+    #1
 sort data.txt | uniq -u
-#------
+    #FLAG
 UsvVyFSfZZWbi6wgC7dAFyFuR6jQQUhR
-#Bandit Lvl 9
+
+            ####BANDIT LVL 9 <----
 strings data.txt | grep "===" | awk 'NR==4 {print $2}'
-#------
+    #------
 strings data.txt | grep "===" | awk '{print $2}' | tail -n 1
-#------
+    #FLAG
 truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk
+
+            ####BANDIT LVL 10 <----
+
